c254361bf81c0c6665bd6b39e93a0680921c0f62
Majo <76714101+carbotton@users.noreply.github.com>
2023-05-13 20:26:11 -0300

    comments

--- Procesamiento de Imagenes/filtrado.py
+++ Procesamiento de Imagenes/filtrado.py
@@ -15,7 +15,7 @@ cv2.imshow('original',img)
 
 kernel_3x3=np.ones((3,3),np.float32)/(3*3)
 output=cv2.filter2D(img,-1,kernel_3x3)
-cv2.imshow('promedio 3x3',output)
+cv2.imshow('promedio 3x3',output) # mostrar imagen original
 
 kernel_5x5=np.ones((5,5),np.float32)/(5*5)
 output=cv2.filter2D(img,-1,kernel_5x5)
@@ -23,9 +23,9 @@ cv2.imshow('promedio 5x5',output)
 
 kernel_31x31=np.ones((31,31),np.float32)/(31*31)
 output=cv2.filter2D(img,-1,kernel_31x31)
-cv2.imshow('promedio 31x31',output)
+cv2.imshow('promedio 31x31',output) # aca ya se ve bien difuminado
 
-output=cv2.GaussianBlur(img,(3,3),0)
+output=cv2.GaussianBlur(img,(3,3),0)  # filtro gaussiano es el que mas se usa para ruido en imagenes
 cv2.imshow('Gauss dev=3x3',output)
 
 output=cv2.GaussianBlur(img,(11,11),0)
